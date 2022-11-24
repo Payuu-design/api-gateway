fcd4ab803b7dde8d08cc1df9639d502d188a7c23
vergaraldvm <leonardo11vergara11@gmail.com>
2022-11-24 16:13:10 -0500

    asdasd

--- src/routes/enable.routes.js
+++ src/routes/enable.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import enable from '../controllers/enable.controllers.js';
+import { enable, enablePOST } from '../controllers/enable.controllers.js';
 
 const router = Router();
 
 router.put('/:id', enable);
-router.post('/:id', enable);
+router.post('/:id/value/:value', enablePOST);
 
 export default router;
